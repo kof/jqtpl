5a3a4ab6cfd54f7cbe6d7c71cb8490e6bfea9042
Oleg Slobodskoi @oleg008 <oleg008@gmail.com>
2010-11-07 02:11:44 +0100

    Closes #2. Now are the most options for render method of express are supported.

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 var a = require( "assert" ),
-    jte = require( "./../lib/jqtpl" );
+    jte = require( "./../lib/jqtpl" ),
+    undefined;
 
 var tpl1 = "<div>${a}</div>",
     tpl2 = "<div>{{= a}}</div>",
@@ -17,45 +18,46 @@ var tpl1 = "<div>${a}</div>",
 
 // "template" method
 a.equal( typeof jte.template("test", tpl1), "function", "precompile template and cache using template name" );
-a.equal( jte.render("test", {a:1}), "<div>1</div>", "render using template name" );
+a.equal( jte.tmpl("test", {a:1}), "<div>1</div>", "render using template name" );
 a.ok( delete jte.template["test"], "remove cache item" );
 
 // ${}
-a.equal( jte.render(tpl1, {a:1}), "<div>1</div>", "use simple data object" );
-a.equal( jte.render(tpl1,{a:'<div id="123">2</div>'}), "<div>&lt;div id=&quot;123&quot;&gt;2&lt;/div&gt;</div>", "escaping per default" );
-a.equal( jte.render(tpl2, {a:1}), "<div>1</div>", "use simple data object, using {{= }} in the template " );
-a.equal( jte.render(tpl1, [{a:1}]), "<div>1</div>", "use an array with one object element" );
-a.equal( jte.render(tpl2, [{a:1}]), "<div>1</div>", "use an array with one object element, using {{= }} in the template" );
-a.equal( jte.render(tpl1, [{a:1},{a:2}]), "<div>1</div><div>2</div>", "use an array with 2 objects" );
-a.equal( jte.render(tpl1, {a: function(){return 1}}), "<div>1</div>", "use function as a value" );
+a.equal( jte.tmpl(tpl1, {a:1}), "<div>1</div>", "use simple data object" );
+a.equal( jte.tmpl(tpl1,{a:'<div id="123">2</div>'}), "<div>&lt;div id=&quot;123&quot;&gt;2&lt;/div&gt;</div>", "escaping per default" );
+a.equal( jte.tmpl(tpl2, {a:1}), "<div>1</div>", "use simple data object, using {{= }} in the template " );
+a.equal( jte.tmpl(tpl1, [{a:1}]), "<div>1</div>", "use an array with one object element" );
+a.equal( jte.tmpl(tpl2, [{a:1}]), "<div>1</div>", "use an array with one object element, using {{= }} in the template" );
+a.equal( jte.tmpl(tpl1, [{a:1},{a:2}]), "<div>1</div><div>2</div>", "use an array with 2 objects" );
+a.equal( jte.tmpl(tpl1, {a: function(){return 1}}), "<div>1</div>", "use function as a value" );
 
 // local variables
-a.equal( jte.render(tpl3, {a:1}), "<div>1</div>", "test access to $data" );
-a.equal( jte.render(tpl4, {a:1}), "<div>1</div>", "test access to $item" );
-a.equal( jte.render(tpl5, null, {someFunction: function() {return 1}}), "<div>1</div>", "test access to $item" );
+a.equal( jte.tmpl(tpl3, {a:1}), "<div>1</div>", "test access to $data" );
+a.equal( jte.tmpl(tpl4, {a:1}), "<div>1</div>", "test access to $item" );
+a.equal( jte.tmpl(tpl5, null, {someFunction: function() {return 1}}), "<div>1</div>", "test access to $item" );
 
 // ${html}
-a.equal( jte.render(tpl6,{a:'<div id="123">2</div>'}), '<div id="123">2</div>', 'output html without escaping');
+a.equal( jte.tmpl(tpl6,{a:'<div id="123">2</div>'}), '<div id="123">2</div>', 'output html without escaping');
 
 
 // ${if}
-a.equal( jte.render(tpl9,{a:1}), "<div>1</div>", "test 'if' when true" );
-a.equal( jte.render(tpl9,{a:2}), "", "test 'if' when false" );
+a.equal( jte.tmpl(tpl9,{a:1}), "<div>1</div>", "test 'if' when true" );
+a.equal( jte.tmpl(tpl9,{a:2}), "", "test 'if' when false" );
 
 // ${else}
-a.equal( jte.render(tpl11,{a:1}), "<div>1</div>", "test else when true" );
-a.equal( jte.render(tpl11,{a:2}), "2", "test else when false" );
-a.equal( jte.render(tpl12,{a:2}), "2", "test else =2" );
-a.equal( jte.render(tpl12,{a:3}), "3", "test else =3" );
+a.equal( jte.tmpl(tpl11,{a:1}), "<div>1</div>", "test else when true" );
+a.equal( jte.tmpl(tpl11,{a:2}), "2", "test else when false" );
+a.equal( jte.tmpl(tpl12,{a:2}), "2", "test else =2" );
+a.equal( jte.tmpl(tpl12,{a:3}), "3", "test else =3" );
 
 // {{each}}
-a.equal( jte.render(tpl8, {names: ["A", "B"]}), "<div>0.A</div><div>1.B</div>", "test 'each', use index and value, explizitely mapping them " );
-a.equal( jte.render(tpl13, {names: ["A", "B"]}), "<div>0.A</div><div>1.B</div>", "test 'each', use index and name with auto mapping" );
-a.equal( jte.render(tpl7, null, {getData: function(){ return [1,2,3]}}), "<div>1</div><div>2</div><div>3</div>", "test 'each', using templates variables" );
+a.equal( jte.tmpl(tpl8, {names: ["A", "B"]}), "<div>0.A</div><div>1.B</div>", "test 'each', use index and value, explizitely mapping them " );
+a.equal( jte.tmpl(tpl13, {names: ["A", "B"]}), "<div>0.A</div><div>1.B</div>", "test 'each', use index and name with auto mapping" );
+a.equal( jte.tmpl(tpl7, null, {getData: function(){ return [1,2,3]}}), "<div>1</div><div>2</div><div>3</div>", "test 'each', using templates variables" );
 
 // {{tmpl}}
-a.equal( jte.render(tpl4, {a:1, extTpl: tpl1}), "<div>1</div>", "include template" );
+a.equal( jte.tmpl(tpl4, {a:1, extTpl: tpl1}), "<div>1</div>", "include template" );
 
+/*
 // {{wrap}}
 
 var tpl1 = 
@@ -79,12 +81,49 @@ var tpl2 =
         </tr>\
     </tbody></table>';
 
-//jte.template( "#tableWrapper", tpl2 );
-//console.log(jte.render(tpl1));
+jte.template( "#tableWrapper", tpl2 );
+console.log(jte.tmpl(tpl1));
+*/
 
 // {{!}}
-a.equal( jte.render("<div>{{! its a comment}}</div>", {a:1}), "<div></div>", "comments work" );
+a.equal( jte.tmpl("<div>{{! its a comment}}</div>", {a:1}), "<div></div>", "comments work" );
 
+
+// Express render method
+
+a.equal( 
+    jte.render( tpl1, { locals: {a:1}, cache: false, filename: "test" } ),
+    "<div>1</div>",
+    "express locals test" 
+);
+
+a.equal( jte.template["test"], undefined, "express cache and filename test" );
+
+a.equal( 
+    jte.render( "<div>${this.test}</div>", { locals: {a:1}, scope: {test: 123} } ),
+    "<div>123</div>",
+    "express scope test" 
+);
+
+
+
+(function(){
+    var printed,
+        util = require( "util" ),
+        print = util.print;
+    
+    // mock print method
+    util.print = function( str ) {
+        printed = true;
+    };
+    
+    jte.render( tpl1, { locals: {a:1}, debug: true} );
+    
+    // restore orig. print function
+    util.print = print;
+    
+    a.ok( printed, "express debug test" );
+})()
 
 
 require( "util" ).print("Tests passed successfull\n");
