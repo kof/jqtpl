kof/jqtpl

commit c294a4e72945eb70baf390560f2bfebb9784bcb6

Closes #8. Fixed quotes escaping issue.

lib/jqtpl.js
@@ -168,36 +168,13 @@ function tiNest(tmpl, data, options) {
 }
 
 /**
- * Render template
- * @param {string} markup html markup or precompiled markup name.
- * @param {*} data can be used in template as template vars.
- * @param {Object=} options additional options.
- * @return {string} ret rendered markup string.
- * @export
+ * Unescape template.
+ * @param {string} args template.
+ * @return {string} args
  */
-exports.tmpl = function(markup, data, options) {
-    var fn = exports.template(null, markup),
-        dataType,
-        ret;
-
-    data = data || {};
-    options = options || {};
-    options.data = data;
-    options.nest = tiNest;
-
-    if (data instanceof Array) {
-        ret = '';
-        data.forEach(function(data) {
-            options.data = data;
-            ret += fn.call(options.scope, exports, options);
-        });
-    } else {
-        ret = fn.call(options.scope, exports, options);
-    }
-
-    return ret;
-};
-
+function unescape(args) {
+    return args ? args.replace(/\\'/g, "'").replace(/\\"/g, '"').replace(/\\\\/g, '\\') : null;
+}
 
 /**
  * Build reusable function for template generation
@@ -217,6 +194,7 @@ function buildTmplFn(markup) {
             markup
                 .trim()
                 .replace(/([\\'])/g, '\\$1')
+                .replace(/([\\"])/g, '\\$1')
                 .replace(/[\r\t\n]/g, '')
                 .replace(/\$\{([^\}]*)\}/g, '{{= $1}}')
                 .replace(/\{\{(\/?)(\w+|.)(?:\(((?:[^\}]|\}(?!\}))*?)?\))?(?:\s+(.*?)?)?(\(((?:[^\}]|\}(?!\}))*?)\))?\s*\}\}/g,
@@ -235,15 +213,16 @@ function buildTmplFn(markup) {
                         }
 
                         if (target) {
-                            args = args ? (',' + args + ')') : (parens ? ')' : '');
+                            target = unescape(target);
+                            args = args ? (',' + unescape(args) + ')') : (parens ? ')' : '');
                             // Support for target being things like a.toLowerCase();
                             // In that case don't call with template item as 'this' pointer. Just evaluate...
-                            expr = parens ? (target.indexOf('.') > -1 ? target + parens : ('(' + target + ').call($item' + args)) : target;
+                            expr = parens ? (target.indexOf('.') > -1 ? target + unescape(parens) : ('(' + target + ').call($item' + args)) : target;
                             exprAutoFnDetect = parens ? expr : '(typeof(' + target + ')==="function"?(' + target + ').call($item):(' + target + '))';
                         } else {
                             exprAutoFnDetect = expr = def.$1 || 'null';
                         }
-
+                        fnargs = unescape(fnargs);
                         return '");' +
                             tag[slash ? 'close' : 'open']
                                 .split('$notnull_1').join(target ? 'typeof(' + target + ')!=="undefined" && (' + target + ')!=null' : 'true')
@@ -297,4 +276,35 @@ exports.clearCache = function() {
             delete cache[name];
         }
     }
+};
+
+/**
+ * Render template
+ * @param {string} markup html markup or precompiled markup name.
+ * @param {*} data can be used in template as template vars.
+ * @param {Object=} options additional options.
+ * @return {string} ret rendered markup string.
+ * @export
+ */
+exports.tmpl = function(markup, data, options) {
+    var fn = exports.template(null, markup),
+        dataType,
+        ret;
+
+    data = data || {};
+    options = options || {};
+    options.data = data;
+    options.nest = tiNest;
+
+    if (data instanceof Array) {
+        ret = '';
+        data.forEach(function(data) {
+            options.data = data;
+            ret += fn.call(options.scope, exports, options);
+        });
+    } else {
+        ret = fn.call(options.scope, exports, options);
+    }
+
+    return ret;
 };

test/test.js
@@ -17,6 +17,10 @@ a.equal( typeof jte.template("test", tpl1), "function", "precompile template and
 a.equal( jte.tmpl("test", {a:1}), "<div>1</div>", "render using template name" );
 a.ok( delete jte.template["test"], "remove cache item" );
 
+// escaping
+a.equal(jte.tmpl("<div class='test'>test</div>"), "<div class=\\'test\\'>test</div>", 'single quotes');
+a.equal(jte.tmpl('<div class="test">test</div>'), '<div class="test">test</div>', 'double quotes');
+
 // ${}
 a.equal( jte.tmpl(tpl1, {a:1}), "<div>1</div>", "use simple data object" );
 a.equal( jte.tmpl(tpl1,{a:'<div id="123">2</div>'}), "<div>&lt;div id=&quot;123&quot;&gt;2&lt;/div&gt;</div>", "escaping per default" );

commit 2f768925e86e283ce6cc65e3cc0b971dddb34579

fixed regression introduced by c294a4e72945eb70baf390560f2bfebb9784bcb6 - escaping of single quotes

lib/jqtpl.js
@@ -193,7 +193,6 @@ function buildTmplFn(markup) {
             // Convert the template into pure JavaScript
             markup
                 .trim()
-                .replace(/([\\'])/g, '\\$1')
                 .replace(/([\\"])/g, '\\$1')
                 .replace(/[\r\t\n]/g, '')
                 .replace(/\$\{([^\}]*)\}/g, '{{= $1}}')

test/test.js
@@ -18,8 +18,9 @@ a.equal( jte.tmpl("test", {a:1}), "<div>1</div>", "render using template name" )
 a.ok( delete jte.template["test"], "remove cache item" );
 
 // escaping
-a.equal(jte.tmpl("<div class='test'>test</div>"), "<div class=\\'test\\'>test</div>", 'single quotes');
+a.equal(jte.tmpl("<div class='test'>test</div>"), "<div class='test'>test</div>", 'single quotes');
 a.equal(jte.tmpl('<div class="test">test</div>'), '<div class="test">test</div>', 'double quotes');
+a.equal(jte.tmpl("<script>var something = '${myvar}';</script>", {myvar: 'whatever'}), "<script>var something = 'whatever';</script>");
 
 // ${}
 a.equal( jte.tmpl(tpl1, {a:1}), "<div>1</div>", "use simple data object" );