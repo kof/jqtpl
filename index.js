43853f5d09fab91a4ae16f5fa0549f218ef949af
Oleg Slobodskoi <oleg008@gmail.com>
2013-01-02 12:20:41 +0100

    #53 only require lib/express.js if express is in the users dependencies and has the correct version, otherwise warn.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-module.exports = require('./lib/jqtpl');
-module.exports.__express = require('./lib/express').render;
+module.exports = exports = require('./lib/jqtpl');
+
+exports.version = require('./package.json').version;
+
+var express;
+
+try {
+    express = require('express');
+    if (parseInt(express.version.split('.')[0], 10) < 3)) {
+        exports.__express = require('./lib/express').render;
+    } else {
+        exports.__express = function() {
+            console.warn('Express 3.x required.');
+        };
+    }
+} catch(err) {}
